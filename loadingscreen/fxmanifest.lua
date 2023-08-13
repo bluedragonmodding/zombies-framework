e27fac3fdb78d477eacd381943b65be1af6a46d6
A Shaw <antoniobailey1231@outlook.com>
2023-08-13 12:36:24 +1200

    a

--- loadingscreen/fxmanifest.lua
+++ loadingscreen/fxmanifest.lua
@@ -1,10 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
 
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+author 'Blue dragon modding'
+description 'Zombies loadingscreen'
+version '0.0.0'
 
-loadscreen 'index.html'
 
-
-files {
+client_script   
     'index.html',
     'stylesheet.css',
     'config.js',
@@ -19,4 +21,4 @@ files {
     'imgs/bg6.png', 
     'imgs/bg7.png',
     'imgs/logo.png'
-}
+
