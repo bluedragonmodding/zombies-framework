f26dca619a6f68892a90c597c320554876a0b866
A Shaw <antoniobailey1231@outlook.com>
2023-08-13 16:09:34 +1200

    a

--- loadingscreen/config.js
+++ loadingscreen/config.js
@@ -5,14 +5,13 @@ var config = {
     "imgInterval": 8000,
     "music": true,
     /* Insert the Youtube Video ID below */
-    "videoID": "zQciMy_Pr0Q",
+    "videoID": "E9T78bT26sk",
     /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
     "musicVolume": 70,
     "text": {
         "title": "Blue Dragon Modding Zombies Test Server",
         "link": "|Created by That Ziv | Re-edited by Blue Dragon Modding Team | ",
-        "Music": "The Rock - Face Off",
-    },
+        "music": "Tech N9ne - Face Off (feat. Joey Cool, King Iso & Dwayne Johnson)",},
     /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
     "images": ['bg0.png', 'bg1.png', 'bg2.png', 'bg3.png', 'bg4.png', 'bg5.png', 'bg6.png', 'bg7.png']
 }
