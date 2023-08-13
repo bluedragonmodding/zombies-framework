0616ec57b021975a10179cb0f3697daf5dd5eea2
A Shaw <antoniobailey1231@outlook.com>
2023-08-13 18:51:47 +1200

    Update player.lua

--- [main]/player.lua
+++ [main]/player.lua
@@ -1,14 +1,14 @@
 --give weapons and ammo and armor to player when they spawns--
 
-ped = GetPlayerPed(-1)
-GiveWeaponToPed(ped, GetHashKey("WEAPON_PISTOL"), 100, false, false),
-GiveWeaponToPed(ped, GetHashKey("WEAPON_CARBINERIFLE"), 100, false, false),
-GiveWeaponToPed(ped, GetHashKey("WEAPON_PUMPSHOTGUN"), 100, false, false),
-GiveWeaponToPed(ped, GetHashKey("WEAPON_KNIFE"), 100, false, false),
-GiveWeaponToPed(ped, GetHashKey("WEAPON_FLASHLIGHT"), 100, false, false),
+--ped = GetPlayerPed(-1)
+--GiveWeaponToPed(ped, GetHashKey("WEAPON_PISTOL"), 100, false, false),
+--GiveWeaponToPed(ped, GetHashKey("WEAPON_CARBINERIFLE"), 100, false, false),
+--GiveWeaponToPed(ped, GetHashKey("WEAPON_PUMPSHOTGUN"), 100, false, false),
+--GiveWeaponToPed(ped, GetHashKey("WEAPON_KNIFE"), 100, false, false),
+--GiveWeaponToPed(ped, GetHashKey("WEAPON_FLASHLIGHT"), 100, false, false),
 
 --spawns points for player to spawn at--
-local spawnPos = vector3(686.245, 577.950, 130.461)
+--local spawnPos = vector3(686.245, 577.950, 130.461)
 
 AddEventHandler('onClientGameTypeStart', function()
     exports.spawnmanager:setAutoSpawnCallback(function()
